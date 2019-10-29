6d51446bd1b10e0802420c25370214d5bab0c85c
Nkurumwa <nkurumwa51@gmail.com>
2019-10-29 17:05:12 +0300

    adds flash message

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '2b471c51fd96d28fa3ec'
 
 posts = [
     {
@@ -18,6 +21,30 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About ')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        # hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        # user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        # db.session.add(user)
+        # db.session.commit()
+        flash(f'Account created successfully for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'nkurumwa51@gmail.com' and form.password.data == 'password':
+            flash('Logged in successfully!','success')
+            return redirect(url_for('home'))
+        else:
+            flash('Incorrect details. Kindly ensure you input correct details', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 if __name__=='__main__':
     app.run(debug=True)
